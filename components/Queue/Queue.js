0f319e675363c8638c745b27c917970bea9fe46d
DeMarcus Kennedy <demarcus@demarcuss-mbp.lan>
2020-07-13 18:06:16 -0500

    moving to other computer

--- components/Queue/Queue.js
+++ components/Queue/Queue.js
@@ -104,27 +104,96 @@ function Queue({ queue, selectQueue }) {
   return (
     <TouchableOpacity
       onPress={() => {
+        alert(queue.active);
         selectQueue(queue.id);
       }}
     >
-      <View style={styles.QueueContainer}>
+      <View
+        style={
+          queue.active === true
+            ? styles.QueueContainer
+            : styles.QueueContainerInactive
+        }
+      >
         <View style={styles.metaDataContainer}>
-          <Text style={styles.titleText}>{queue.title}</Text>
+          <Text
+            style={
+              queue.active === true
+                ? styles.titleText
+                : styles.titleTextInactive
+            }
+          >
+            {queue.title}
+          </Text>
           {queue.message ? (
-            <Text style={styles.messageText}>{queue.message}</Text>
+            <Text
+              style={
+                queue.active === true
+                  ? styles.messageText
+                  : styles.messageTextInactive
+              }
+            >
+              {queue.message}
+            </Text>
           ) : null}
           <View style={styles.addressContainer}>
-            <Text style={styles.addressText}>{queue.address}, </Text>
-            <Text style={styles.addressText}>{queue.zipCode}</Text>
+            <Text
+              style={
+                queue.active === true
+                  ? styles.addressText
+                  : styles.addressTextInactive
+              }
+            >
+              {queue.address},{" "}
+            </Text>
+            <Text
+              style={
+                queue.active === true
+                  ? styles.addressText
+                  : styles.addressTextInactive
+              }
+            >
+              {queue.zipCode}
+            </Text>
           </View>
           <View style={styles.hoursContainer}>
-            <Text style={styles.hoursText}>{getOpen()}</Text>
-            <Text style={styles.hoursText}>{getClose()}</Text>
+            <Text
+              style={
+                queue.active === true
+                  ? styles.hoursText
+                  : styles.hoursTextInactive
+              }
+            >
+              {getOpen()}
+            </Text>
+            <Text
+              style={
+                queue.active === true
+                  ? styles.hoursText
+                  : styles.hoursTextInactive
+              }
+            >
+              {getClose()}
+            </Text>
           </View>
         </View>
-        <View style={styles.queueCountContainer}>
+        <View
+          style={
+            queue.active === true
+              ? styles.queueCountContainer
+              : styles.queueCountContainerInactive
+          }
+        >
           <Text style={styles.queueCountText}>{queue.count}</Text>
-          <Text style={styles.queueCountTextSub}>{"in line"}</Text>
+          <Text
+            style={
+              queue.active === true
+                ? styles.queueCountTextSub
+                : styles.queueCountTextSubInactive
+            }
+          >
+            {"in line"}
+          </Text>
         </View>
       </View>
     </TouchableOpacity>
@@ -155,9 +224,37 @@ const styles = StyleSheet.create({
     elevation: 5,
     borderRadius: 9,
   },
+  QueueContainerInactive: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 15,
+    backgroundColor: "#eee",
+    borderBottomWidth: 1,
+    borderColor: "#eee",
+    marginBottom: 15,
+    marginRight: 15,
+    marginLeft: 15,
+    marginTop: 15,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+    borderRadius: 9,
+  },
   titleText: {
     fontSize: 18,
     color: "black",
+    fontWeight: "600",
+  },
+  titleTextInactive: {
+    fontSize: 18,
+    color: "#bbb",
     fontWeight: "600",
   },
   messageText: {
@@ -167,9 +264,21 @@ const styles = StyleSheet.create({
     marginTop: 15,
     marginBottom: 10,
   },
+  messageTextInactive: {
+    fontSize: 15,
+    color: "#bbb",
+    fontWeight: "300",
+    marginTop: 15,
+    marginBottom: 10,
+  },
   hoursText: {
     fontSize: 12,
     color: "salmon",
+    fontWeight: "900",
+  },
+  hoursTextInactive: {
+    fontSize: 12,
+    color: "rgba(250, 128, 114, 0.32)",
     fontWeight: "900",
   },
   hoursContainer: {
@@ -185,6 +294,11 @@ const styles = StyleSheet.create({
   addressText: {
     fontSize: 15,
     color: "grey",
+    fontWeight: "300",
+  },
+  addressTextInactive: {
+    fontSize: 15,
+    color: "#bbb",
     fontWeight: "300",
   },
   metaDataContainer: {
@@ -203,6 +317,16 @@ const styles = StyleSheet.create({
     minHeight: 70,
     backgroundColor: "#8ecfd4",
   },
+  queueCountContainerInactive: {
+    // borderStyle: "solid",
+    // borderColor: "red",
+    color: "white",
+    // borderWidth: 0.5,
+    borderRadius: 5,
+    minWidth: 70,
+    minHeight: 70,
+    backgroundColor: "rgba(142, 207, 212, 0.3)",
+  },
   queueCountText: {
     fontSize: 30,
     padding: 10,
@@ -215,6 +339,13 @@ const styles = StyleSheet.create({
     fontSize: 12,
     textAlign: "center",
     color: "yellow",
+    // color: "#fefefe",
+    fontWeight: "500",
+  },
+  queueCountTextSubInactive: {
+    fontSize: 12,
+    textAlign: "center",
+    color: "rgba(255, 255, 0, 0.32)",
     // color: "#fefefe",
     fontWeight: "500",
   },
